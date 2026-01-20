9f03c3823f22c206d0bc42176a52909d1b6b8d2a
Nikita <nikita.kashkin@student.put.poznan.pl>
2026-01-20 16:33:10 +0100

    zad2

--- lab10/lab10.cpp
+++ lab10/lab10.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 void insertNode(int key, string data);
 void displayList();
+void menu();
+void usunElement();
 
 struct Node { //kod 10_01
     int key = -1;
@@ -30,19 +32,40 @@ void insertNode(int key, string data) {
 }
 
 void displayList() {
+    int num = 1;
     Node* temp = head;
     while (temp != nullptr) {
-        cout << "Key: " << temp->key << ", Data: " << temp->data << endl;
+        cout << "<" << num << ">" << " Key: " << temp->key << ", Data: " << temp->data << endl;
         temp = temp->next;
+        num++;
     }
 }
 
 int main() {
     int n, key;
     string data;
-    cout << "Enter the number of nodes: ";
-    cin >> n;
+    //cout << "Enter the number of nodes: ";
+    //cin >> n;
 
+    while(true){
+        menu();
+		cin >> n;
+        switch (n) {
+        case 1://stworz lub dodaj element do tabeli
+            cout << "Podaj klucz: ";
+            cin >> key;
+            cout << "Podaj date: ";
+            cin >> data;
+            insertNode(key, data);
+			break;
+        case 2://usun element
+			displayList();
+        }
+        
+
+    }
+
+    /*
     for (int i = 0; i < n; i++) {
         cout << "Enter key and data for node " << i + 1 << ": " << endl;
         cout << "Please write integer number: ";
@@ -51,7 +74,56 @@ int main() {
         cin >> data;
         insertNode(key, data);
     }
+    */
 
     cout << "The entire list:" << endl;
     displayList();
+}
+
+void deleteElement(Node* elementToRemov) {
+    int index;
+	elementToRemov = head;
+
+	cout << "Podaj numer elementu do usuniecia: ";
+    cin >> index;
+
+    for (int i = 0; i < index; i++) {
+        if (elementToRemov->next != nullptr) {
+            elementToRemov = elementToRemov->next;
+        }
+	}
+
+    if (elementToRemov == head || elementToRemov -> next == nullptr) {
+        head = nullptr;
+        lastAdded = nullptr;
+		delete elementToRemov;
+    }
+    else if (elementToRemov == head || elementToRemov -> next != nullptr) {
+        head = elementToRemov -> next;
+		delete elementToRemov;
+    }
+    else if (elementToRemov != head || elementToRemov->next == nullptr) {
+		Node* temp = head;
+        while (temp->next != elementToRemov) {
+            temp = temp->next;
+        }
+		temp->next = nullptr;
+        lastAdded = temp;
+        delete elementToRemov;
+    }
+    else if (head != elementToRemov || elementToRemov->next != nullptr) {
+        Node* temp = head;
+        while (temp->next != elementToRemov) {
+            temp = temp->next;
+        }
+        temp->next = elementToRemov->next;
+		delete elementToRemov;
+    }
+}
+
+void menu(){
+    cout << "1. Dodaj nowy element" << endl;
+    cout << "2. ususn element" << endl;
+    cout << "3. pokaz liste" << endl;
+    cout << "9. wyjdz z programu ";
 }
